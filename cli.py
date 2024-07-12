40903b7b0571cebbc505154fa3d58d8b0bd2a2f2
Juan Francisco Herrero <juanfraherrero@gmail.com>
2024-07-12 20:34:31 -0300

    Change main to cli filename

--- cli.py
+++ cli.py
@@ -13,9 +13,9 @@ parser.add_argument('-n', '--numberimages', type=int, help='Quantity of images p
 parser.add_argument('-r', '--numberRotations', type=int, help='Quantity of ratations. || Cantidad de giros.', required=False, default=5)
 parser.add_argument('-a', '--anglePerRotation', type=int, help='Angle difference between two rotations. In ° || Ángulo de diferencia entre dos rataciones (cuanto se eleva por cada rotación). En °.', required=False, default=10)
 parser.add_argument('-d', '--distanceOrRadio', type=int, help='Distance between camera and object. || Distancia entre la cámara y el objeto.', required=False, default=60)
-parser.add_argument('-im', '--initMovement', type=int, help='The angle where starts the movement for each rotation. || El ángulo donde arranca el movimiento para cada rotación', required=False, default=0)
-parser.add_argument('-em', '--endMovement', type=int, help='The angle where ends the movement for each rotation. || El ángulo donde termina el movimiento para cada rotación', required=False, default=0)
-parser.add_argument('-csm', '--cantStepMovement', type=int, help='The quantity of movements between angles defined in movement. || La cantidad de movimiento entre los ángulso de movimiento', required=False, default=1)
+parser.add_argument('--initMovement', type=int, help='The angle where starts the movement for each rotation. || El ángulo donde arranca el movimiento para cada rotación', required=False, default=0)
+parser.add_argument('--endMovement', type=int, help='The angle where ends the movement for each rotation. || El ángulo donde termina el movimiento para cada rotación', required=False, default=0)
+parser.add_argument('--cantStepMovement', type=int, help='The quantity of movements between angles defined in movement. || La cantidad de movimiento entre los ángulso de movimiento', required=False, default=1)
 parser.add_argument('-v', '--verbose', help='Show info of steps. || Mostrar información de los pasos', action='store_true', required=False, default=False)
 args = parser.parse_args()
 
