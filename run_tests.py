6db4515e3034012fda75887b22a805f3706f4768
Juan Francisco Herrero <juanfraherrero@gmail.com>
2024-07-25 18:59:01 -0300

    ✅Add unit test to check pipeline CI/CD

--- run_tests.py
+++ run_tests.py
@@ -1,7 +1,10 @@
+import sys
 import unittest
 
 if __name__ == "__main__":
     loader = unittest.TestLoader()
     suite = loader.discover(start_dir="./test", pattern="test_*.py")
     runner = unittest.TextTestRunner()
-    runner.run(suite)
+    result = runner.run(suite)
+
+    sys.exit(0 if result.wasSuccessful() else 1)
